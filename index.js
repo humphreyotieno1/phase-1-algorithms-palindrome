472c7af7d03e0d701dfacc9a6b4f59ebfe264095
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-21 10:52:32 +0300

    Algorithms and problem solving

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const lowerCaseWord = word.toLowerCase();
+  let left = 0;
+  let right= lowerCaseWord.length -1;
+
+  while (left < right) {
+    if (lowerCaseWord[left] !== lowerCaseWord[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  Pseudocode
+  - Convert input to lowercase
+  - Initialise two pointers, one at the start and one at the end of the word
+  - While the left is less than the right pointer:
+    - If characters at current pointers are not equal, return false (not a palindrome)
+    - Move the left pointer to the right and right to the left
+  - If the loop completes without returning false, return true (a palindrome)
 */
 
 /*
-  Add written explanation of your solution here
+  The function uses a case-insensitive approach by converting the input word to lowercase.
+  Then, it employs a two-pointer technique to compare characters from the 
+  beginning and end of the word simultaneously, moving towards the center. 
+  If at any point the characters at the current pointers are not equal, 
+  the function concludes that the word is not a palindrome and returns false. Otherwise, 
+  if the loop completes without returning false, the word is recognized 
+  as a palindrome, and the function returns true.
 */
 
 // You can run `node index.js` to view these console logs
